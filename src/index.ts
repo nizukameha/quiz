75c543f25642be2a527cae3f7d9c94341a9cf3db
Lexa <axel.reviron@gmail.com>
2022-12-12 21:14:29 +0100

    add class for selected answer

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ let score = document.querySelector<HTMLElement>('.score');
 let scoreNumber = 0;
 let valider = document.querySelector<HTMLElement>('.valider');
 let reponse1 = document.querySelector<HTMLElement>('.reponse1');
+let reponse2 = document.querySelector<HTMLElement>('.reponse2');
+let reponse3 = document.querySelector<HTMLElement>('.reponse3');
+let reponse4 = document.querySelector<HTMLElement>('.reponse4');
 
 if (reponse1) {
-    console.log('OK');
     reponse1.onclick = changeColor;
 }
+
 // Cette fonction permet de changer la couleur d'une réponse selectionnée
 function changeColor () {
     if (reponse1) {
-    reponse1.style.backgroundColor = 'blue';
+    reponse1.classList.add('reponseSelect');
     }
 }
 
