7d1836b01146ed668e971a2a7f22278289c5321d
Lexa <axel.reviron@gmail.com>
2022-12-23 14:36:37 +0100

    add page for the end of the quiz

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// PROBLEME LORSQUON CLIQUE 2 FOIS SUR VALIDER
-
 /*----------
 IMPORT
 ---------- */
@@ -34,6 +32,9 @@ let timeHTML = document.querySelector<HTMLElement>('.time');
 let commencer = document.querySelector<HTMLButtonElement>('.commencer');
 let commencement = document.querySelector<HTMLDivElement>('.commencement');
 let mainSection = document.querySelector<HTMLDivElement>('.mainSection');
+let commencementH2 = document.querySelector<HTMLHeadingElement>('.commencementH2');
+let commencementUl = document.querySelector<HTMLUListElement>('.commencementUl');
+let commencementP = document.querySelector<HTMLParagraphElement>('.commencementP')
 // Boolean
 let isCorrectAnswer: boolean = false;
 let answerIsSelected: boolean = false;
@@ -67,6 +68,7 @@ let tabGoodAnswer: string[] = ['Kentaro Miura', '1984', '250 000 000', 'Irumi', 
 CONDITIONS
 ---------- */
 
+// Affiche les réponses actuelles
 for (const reponse of reponses) {
     reponse.innerHTML = tabReponses[0][i];
     i++;
@@ -87,11 +89,12 @@ if (question) {
 if (score) {
     score.innerHTML = scoreNumber + '';
 }
+// Affiche le temps pour le timer
 if (timeHTML) {
     timeHTML.innerHTML = String(timeCounter);
 }
 
-// Lors du click du btn 
+// Lors du click sur le boutton 'Commencer' 
 commencer?.addEventListener('click', () => {
     commencer?.classList.add('hide')
     commencement?.classList.add('hide')
@@ -99,11 +102,7 @@ commencer?.addEventListener('click', () => {
     timer();
 })
 
-// Si 'valider' existe, la fonction 'check' se lance lorsqu'on clique sur le bouton
-// BUG LORSQUON CLIQUE RAPIDEMENT SUR VALIDER
-// TIMER QUI DECLENCHE LES ACTIONS DE VALIDER SANS CLIQUER DESSUS
-// PAGE DE FIN DE QUIZ
-// SI UNE BONNE REPONSE FONCTIONNE TOUT LE TEMPS AVEC LE TIMER
+// Lors du click sur le boutton 'Valider' des fonctions sont appellées pour enchainer les questions/réponses
 valider?.addEventListener('click', () => {
     if (answerIsSelected == true) {
         if (valider) {
@@ -112,20 +111,9 @@ valider?.addEventListener('click', () => {
         check();
         AnswerCheckColor();
         if (questionCounter < 9) {
-            setTimeout(next, 2000, goodAnswerCounter++, 2000);
-            setTimeout(changeAnswerOnValidate, 2000);
-            setTimeout(removeClass, 2000);
-            setTimeout(validerDisabledFalse, 2000);
+            quiz()
         } else {
-            for (const reponse of reponses) {
-                reponse.style.display = 'none';
-            }
-            if (valider) {
-                valider.style.display = 'none';
-            }
-            if (question) {
-                question.style.display = 'none';
-            }
+            quizEnd();
         }
     }
 })
@@ -149,6 +137,42 @@ for (const reponse of reponses) {
 /*----------
 FONCTIONS
 ---------- */
+
+/**
+ * Change la couleur des réponses. Passes aux questions/réponses suivantes
+ */
+function quiz() {
+    setTimeout(()=>{
+        next();
+        goodAnswerCounter++;
+        changeAnswerOnValidate();
+        removeClass();
+        validerDisabledFalse();
+    },1500)
+}
+
+/**
+ * Cache des éléments html et en affiche d'autres pour la fin du quiz
+ */
+function quizEnd() {
+    setTimeout(()=>{
+        for (const reponse of reponses) {
+            reponse.classList.add('hide');
+        }
+        valider?.classList.add('hide');
+        question?.classList.add('hide');
+        mainSection?.classList.add('hide');
+        commencer?.classList.remove('hide');
+        commencement?.classList.remove('hide');
+        if (commencementH2 && commencer && commencementP) {
+            commencementH2.innerHTML = 'Fin du quiz !';
+            commencementP.innerHTML = 'Merci d\'avoir participé a mon quiz :)<br>Vous pouvez trouver mes autres projets ici :<br><a href="https://axel-reviron.netlify.app/"><img src="https://axel-reviron.netlify.app/img/logo-h-purple-fonce.webp" alt="portfolio" style="width: 50%; margin-top: 10%;"></a>'
+            commencementP.style.marginTop = '10%';
+        }
+        commencer?.classList.add('hide');
+        commencementUl?.classList.add('hide');
+    }, 1500)
+}
 
 /**
  * Le bouton pour valider la réponse est de nouveau disponible
@@ -181,19 +205,9 @@ function timer() {
             }
             AnswerCheckColor();
             if (questionCounter < 9) {
-                setTimeout(next, 2000, goodAnswerCounter++, 2000);
-                setTimeout(changeAnswerOnValidate, 2000);
-                setTimeout(removeClass, 2000);
+                quiz()
             } else {
-                for (const reponse of reponses) {
-                    reponse.style.display = 'none';
-                }
-                if (valider) {
-                    valider.style.display = 'none';
-                }
-                if (question) {
-                    question.style.display = 'none';
-                }
+                quizEnd();
             }
         }
     }
@@ -219,8 +233,9 @@ function removeClass() {
         reponse.classList.remove('hide');
     }
 }
+
 /**
- * Elle porte bien son nom celle-la
+ * Fait apparaitre les questions et les réponses quand on commence le quiz
  */
 function appear() {
     for (const reponse of reponses) {
